3816a4e98d474cc4c6eec7d3f00b7f07acf2a5d6
ysiguman <victord675@gmail.com>
2017-09-12 18:09:39 +0200

    Add grid and user management

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <vector>
 #include "header.hpp"
 
 class Player
@@ -31,6 +32,74 @@ public:
     }
 };
 
+class Grid {
+private:
+    int L = 7;
+    int h = 6;
+    vector<vector <string>> grid;
+
+    int getRowId(int column) {
+        int i = 0;
+        for (i = 0; i < h; ++i) {
+            if (grid[i][column] != "_  ")
+                return i - 1;
+        }
+        return i - 1;
+    }
+
+    bool checkVertical(int row, string token, int column) {
+        for (int i = 1; i <= 4; ++i) {
+            if (grid[row + i][column] != token)
+                return false;
+        }
+        return true;
+    }
+
+public:
+    Grid() {
+        vector<string> row(L);
+        for (int k = 0; k < h; ++k) {
+            grid.push_back(row);
+        }
+
+        for (int i = 0; i < h; ++i) {
+            for (int j = 0; j < L; ++j) {
+                grid[i][j] = "_  ";
+            }
+        }
+    }
+
+    void drawGrid () {
+        for (int i = 0; i < h + 1; ++i) {
+            for (int j = 0; j < L; ++j) {
+                if (i != h)
+                    cout << grid[i][j];
+                else
+                    cout << j << ": ";
+            }
+            cout << endl;
+        }
+    }
+
+    void setToken(PlayerHuman *player, int column)
+    {
+        grid[this->getRowId(column)][column] = player->getColor() + "  ";
+    }
+
+    bool testColumnValidity(int column) {
+        return (column >= 0 && column <= L - 1 && this->getRowId(column) != -1);
+    }
+
+    bool checkToken(int column) {
+        int lastRow = this->getRowId(column);
+        string lastToken = grid[lastRow + 1][column];
+        if (lastRow < h - 4 && lastToken == grid[lastRow + 2][column]) {
+            return checkVertical(lastRow, lastToken, column);
+        }
+        return false;
+    }
+};
+
 class P4
 {
 private:
@@ -50,7 +119,7 @@ private:
             current = player1;
         }
 
-        cout << "> " << current->getName() << endl;
+        cout << "> " <<  current->getName() << endl;
     }
 
 public:
@@ -75,6 +144,26 @@ public:
         cout << "    and : " << player2->getName() << " " << player2->getColor() << endl;
 
         this->changePlayer();
+
+        Grid *grille = new Grid();
+        grille->drawGrid();
+
+        int column;
+        do {
+            do {
+                cout << current->getColor() << "  " << current->getName() << " column : ";
+                cin >> column;
+            } while (!grille->testColumnValidity(column));
+
+            grille->setToken(current, column);
+            grille->drawGrid();
+            if (grille->checkToken(column)) {
+                cout << current->getName() << " est le winner" << endl;
+                break;
+            }
+
+            this->changePlayer();
+        } while (column != -1);
     }
 };
 
