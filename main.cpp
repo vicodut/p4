60dc996b438e3006343942ba5625020605e12901
ysiguman <victord675@gmail.com>
2017-09-12 07:28:17 +0200

    Get / Set players

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@ class PlayerHuman
 {
 private:
     string name;
+    string color;
 public:
     PlayerHuman() {}
 
@@ -17,8 +18,16 @@ public:
         return name;
     }
 
-    void setNAme(string name) {
+    void setNAme(const string name) {
         this->name = name;
+    }
+
+    const string &getColor() const {
+        return color;
+    }
+
+    void setColor(const string color) {
+        this->color = color;
     }
 };
 
@@ -27,22 +36,45 @@ class P4
 private:
     PlayerHuman *player1;
     PlayerHuman *player2;
+    PlayerHuman *current;
+
+    void changePlayer() {
+        if (current) {
+            if (current == player1) {
+                current = player2;
+            } else {
+                current = player1;
+            }
+        } else {
+            current = new PlayerHuman();
+            current = player1;
+        }
+
+        cout << "> " << current->getName() << endl;
+    }
+
 public:
     P4() {
-        cout << "Welcome to the game  \u25C9 \u25EF" << endl;
         player1 = new PlayerHuman();
         player2 = new PlayerHuman();
+
+        cout << "Welcome to the game  \u25C9 \u25EF" << endl;
 
         string name;
         cout << "Name of Player1 : ";
         cin >> name;
         player1->setNAme(name);
+        player1->setColor("\u25C9");
+
         cout << "Name of Player2 : ";
         cin >> name;
         player2->setNAme(name);
+        player2->setColor("\u25EF");
 
-        cout << "Players : " << player1->getName() << endl;
-        cout << "    and : " << player2->getName() << endl;
+        cout << "Players : " << player1->getName() << " " << player1->getColor() << endl;
+        cout << "    and : " << player2->getName() << " " << player2->getColor() << endl;
+
+        this->changePlayer();
     }
 };
 
